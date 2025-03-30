e47bc6546334654f059d180d62af0967e9a65a7b
Kacper <kulakowski.ka@gmail.com>
2025-03-30 21:12:03 +0200

    Updated files and class names

--- AdventOfCode2024/Day1Part1.cs
+++ AdventOfCode2024/Day1Part1.cs
@@ -8,11 +8,7 @@ namespace AdventOfCode2024
     public class Day1Part1
     {
         DayOneFileData fileData = new();
-        //Read pairs of data into string array. Every index is "12345   12345"
-        // string[] values = FileData.fileData;
-        // //Create Lists to hold parsed data
-        // protected List<int> valuesLeft = new();
-        // protected List<int> valuesRight = new();
+
         //Parse data with Split() method. String we are spliting is "   ", string on the left goes to list values1, string on the right goes to list values2
         public Day1Part1()
         {
@@ -23,14 +19,6 @@ namespace AdventOfCode2024
         int distance = 0;
         int minValue1;
         int minValue2;
-        // protected virtual void ParseData()
-        // {
-        //     foreach(var value in values)
-        //     {
-        //         valuesLeft.Add(Convert.ToInt32(value.Split("   ", StringSplitOptions.None)[0]));
-        //         valuesRight.Add(Convert.ToInt32(value.Split("   ", StringSplitOptions.None)[1]));
-        //     }
-        // }
 
         protected virtual void CalculateDistance()
         {
